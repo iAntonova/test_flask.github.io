bfcd9315162e5908e544966cb1178b0799ce0fe5
iAntonova <antonovava@gmail.com>
2022-01-08 14:38:21 +0200

    15

--- app.py
+++ app.py
@@ -56,6 +56,12 @@ class UserForm(FlaskForm):
     password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+# Create a Form Class
+class PasswordForm(FlaskForm):
+    email = StringField("What's Your Email", validators=[DataRequired()])
+    password_hash = PasswordField("What's Your Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Update Database Record
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
@@ -131,6 +137,33 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", user_name=name)
+
+# Create Password Test Page
+@app.route('/test_pw', methods=['GET', 'POST'])
+def test_pw():
+    email = None
+    password = None
+    pw_to_check = None
+    passed = None
+
+    form = PasswordForm()
+
+    # Validate Form
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password_hash.data
+        # Clear the form
+        form.email.data = ''
+        form.password_hash.data = ''
+
+        # Lookup User By Email Address
+        pw_to_check = Users.query.filter_by(email=email).first()
+
+        # Check Hashed assword
+        passed = check_password_hash(pw_to_check.password_hash, password)
+
+    return render_template("test_pw.html", email = email, password = password, 
+            pw_to_check = pw_to_check, passed = passed, form = form)
 
 
 # Create Custom Error Pages
